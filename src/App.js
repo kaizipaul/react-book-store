557ae1d9be119e13694dda90bf85a6985088e86a
kaizipaul <kaizipaul@gmail.com>
2022-11-04 17:02:43 +0300

    Imported components and added Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/navBar';
+import Categories from './components/Categories';
+import BookSection from './components/BookSection';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<BookSection />} />
+        <Route exact path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
   );
 }
 
