2c63fef15f685a7faccdc835221a5fc4be6fca2a
kaizipaul <kaizipaul@gmail.com>
2022-11-11 16:25:07 +0300

    Connected book data to api

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,26 +1,101 @@
 const ADD_BOOK = 'book-store/books/ADD_BOOK';
 const REMOVE_BOOK = 'book-store/books/REMOVE_BOOK';
+const GET_BOOKS = 'bookstore/books/GET_BOOKS';
+const apiUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/J24AiHVbjsRSy0mbmbHF/books';
 
 const initialState = [];
 
-export const addbook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
+const getFromApi = async () => {
+  const response = await fetch(apiUrl, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const books = await response.json();
+  const booksData = Object.keys(books).map((id) => ({
+    id,
+    title: books[id][0].title,
+    author: books[id][0].author,
+  }));
+  return booksData;
+};
+
+export const fetchBooks = () => (async (dispatch) => {
+  const booksData = await getFromApi();
+  dispatch({
+    type: GET_BOOKS,
+    payload: booksData,
+  });
 });
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
+const sendToApi = async (payload) => {
+  const {
+    id, title, author, category,
+  } = payload;
+  const send = await fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify({
+      item_id: id,
+      title,
+      author,
+      category,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  (await send.text());
+};
+
+const deleteFromApi = async (id) => {
+  const remove = await fetch(`${apiUrl}/${id}`, {
+    method: 'DELETE',
+    body: JSON.stringify({
+      item_id: id,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  (await remove.text());
+};
+
+export const addbook = (payload) => (async (dispatch) => {
+  const { id, title, author } = payload;
+  await sendToApi(payload);
+  dispatch({
+    type: ADD_BOOK,
+    book: { id, title, author },
+  });
+});
+
+export const removeBook = (id) => (async (dispatch) => {
+  await deleteFromApi(id);
+  dispatch({
+    type: REMOVE_BOOK,
+    id,
+  });
+});
+
+export const getbooks = () => (async (dispatch) => {
+  const booksData = await getFromApi();
+  dispatch({
+    type: GET_BOOKS,
+    payload: booksData,
+  });
 });
 
 const bookReducer = (state = initialState, action) => {
-  const { type, payload } = action;
-  switch (type) {
+  switch (action.type) {
     case ADD_BOOK:
-      return [...state, payload];
+      return [...state, action.book];
 
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== payload);
+      return state.filter((book) => book.id !== action.id);
+
+    case GET_BOOKS:
+      return action.payload;
     default:
       return state;
   }
